77e7a4486acd3bf894f7c29c94fc0d44de7c0b6a
dhruv <dhruvgupta742@gmail.com>
2023-08-16 22:26:29 +0530

    experience responsive

--- src/components/Hobbies/card.tsx
+++ src/components/Hobbies/card.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React,{ReactNode} from "react";
 import styles from "./page.module.css"
-import { ArrowRight } from "iconoir-react"
-import Link from "next/link";
-import DriveEtaIcon from '@mui/icons-material/DriveEta';
+
 export interface cardProp {
+    Icon:ReactNode;
     isLast: boolean;
+    h:string;
 };
 
 
-const Card: React.FC<cardProp> = ({isLast})=>{
+const Card: React.FC<cardProp> = ({isLast,h, Icon})=>{
     return (
         <div className={!isLast ? styles.item : styles.itemEnd}>
             <div className={styles.left}>
                 <div className={styles.imageContainer}>
-                    <DriveEtaIcon width={22} height={22} />
+                    {Icon}
                 </div>
-                <p className={styles.pname}>Authify</p>
+                <p className={styles.pname}>{h}</p>
             </div>
         </div>
     )
--- src/components/Hobbies/page.tsx
+++ src/components/Hobbies/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-import Link from "next/link"
 import styles from "./page.module.css"
 import Card, { cardProp } from "./card"
 import { Carousel } from "./carousel"
+import DriveEtaIcon from '@mui/icons-material/DriveEta';
+import SportsTennisIcon from '@mui/icons-material/SportsTennis';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import SailingIcon from '@mui/icons-material/Sailing';
 
 export default function Hobbies() {
     return (
@@ -11,13 +14,15 @@ export default function Hobbies() {
                 <h2 className="section-heading">
                     Hobbies
                 </h2>
-                <Card isLast={false} />
-                <Card isLast={false} />
-                <Card isLast={true} />
+                <Card Icon={<DriveEtaIcon width={22} height={22} />} h="Driving" isLast={false} />
+                <Card Icon={<MusicNoteIcon width={22} height={22} />} h="Music" isLast={false} />
+                <Card Icon={<SailingIcon width={22} height={22} />} h="Travelling" isLast={false} />
+                <Card Icon={<SportsTennisIcon width={22} height={22} />} h="Table Tennis" isLast={true} />
             </div>
             <div className={styles.subcontainer}>
-               <Carousel />
+                {/* <Carousel /> */}
+                <iframe src="https://open.spotify.com/embed/playlist/39d35DN2b1DkxEc2obaXF9?utm_source=generator&theme=0" width="100%"  frameBorder="0" height="352px" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </div>
-    )
-}   
+    ) 
+}    
