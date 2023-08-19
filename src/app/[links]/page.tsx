90f309276152db10313bfb8da0b4b2e59a3c7848
dhruv <dhruvgupta742@gmail.com>
2023-08-19 22:43:30 +0530

    404 fixes

--- src/app/[links]/page.tsx
+++ src/app/[links]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { usePathname, useRouter } from 'next/navigation'
 import linkData from './d_link.json'
 import Link from 'next/link';
+import styles from "../404.module.css";
 
 interface LinkData {
     name: string;
@@ -14,7 +15,7 @@ const Links = () => {
     const pathName = usePathname().split("/")[1];
     const router = useRouter();
     const target = linkData.find((item: LinkData) => item.name === pathName);
-    
+
     if (target) {
         router.push(target.link)
     }
@@ -24,15 +25,15 @@ const Links = () => {
 
     return (
         <div>
-            {target !== undefined ? (<div>
-                <h1>Redirecting to {target.name}...</h1>
-                <p>If the redirection doesnt happen automatically, click <a href={target.link}>here</a>.</p>
-                <Link href="/">
-                    <a>Go Back</a>
+            {target !== undefined ? (<div className={styles.mainContainer}>
+                <h1 className={styles.text}>Redirecting to {target.name}...</h1>
+                <p className={styles.sub}>If the redirection doesnt happen automatically, click <a href={target.link}>here</a>.</p>
+                <Link href="/" className={styles.link}>
+                    Go Back
                 </Link>
             </div>) :
-                <div>
-                    Not Found
+                <div className={styles.mainContainer}>
+                    <p className={styles.text}>Not Found</p>
                 </div>}
         </div>
     )
