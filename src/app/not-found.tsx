16d61ed2043c066ea1fb792a14488668ed71f3ac
dhruv <dhruvgupta742@gmail.com>
2023-08-19 16:37:50 +0530

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import styles from "./404.module.css"
+import type { Metadata } from 'next'
+import Link from "next/link"
+
+export const metadata: Metadata = {
+  title: '404 | Not Found',
+  description: 'Dhruv Gupta | Software Developer | Student at USICT, Guru Gobind Singh Indraprastha University'
+}
+
+export default function NotFound() {
+  return (
+    <>
+      <div className={styles.mainContainer}>
+        <h1 className={styles.heading}>404</h1>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>OOPS! Looks like you&#39;re lost.</p>
+          <p className={styles.sub}>The page you are looking for is not avaible.</p>
+        </div>
+        <Link href={'/'} className={styles.link}>Go Back</Link>
+      </div>
+    </>
+  )
+}
+ 
+ 
