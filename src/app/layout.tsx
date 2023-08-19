8dcad714b02ab2b42efa9a5d8f76c99301c61b5e
dhruv <dhruvgupta742@gmail.com>
2023-08-19 15:36:34 +0530

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,15 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
         <meta property="og:title" content="Dhruv | Portfolio" />
         <meta property="og:description" content="Dhruv Gupta | Software Developer | Student at USICT, Guru Gobind Singh Indraprastha University" />
-        <meta property="og:image" content="/static/dhruv.jpg" />
+        <meta property="og:image" content="/static/temp.jpg" />
         <meta property="og:url" content="mrdhruv.xyz" />
         <meta property="og:type" content="website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="language" content="en"/>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
