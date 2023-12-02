fe6345255ffefc954ce6930c0dc93e5b48e014e3
dhruv <dhruvgupta742@gmail.com>
2023-12-02 12:20:16 +0530

    New project section

--- src/components/Projects/card.tsx
+++ src/components/Projects/card.tsx
@@ -1,26 +1,131 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css"
-import { ArrowRight } from "iconoir-react"
+import { ArrowRight, Cancel } from "iconoir-react"
 import { cardProp } from "./d_projects"
 import Link from "next/link";
 
 
-const Card: React.FC<cardProp> = ({ isLast, url, name, icon }) => {
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useSpring, animated } from '@react-spring/web';
+
+interface FadeProps {
+    children: React.ReactElement;
+    in?: boolean;
+    onClick?: any;
+    onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
+    onExited?: (node: HTMLElement, isAppearing: boolean) => void;
+    ownerState?: any;
+}
+
+const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(props, ref) {
+    const {
+        children,
+        in: open,
+        onClick,
+        onEnter,
+        onExited,
+        ownerState,
+        ...other
+    } = props;
+    const style = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: open ? 1 : 0 },
+        onStart: () => {
+            if (open && onEnter) {
+                onEnter(null as any, true);
+            }
+        },
+        onRest: () => {
+            if (!open && onExited) {
+                onExited(null as any, true);
+            }
+        },
+    });
+
     return (
-        <div className={!isLast ? styles.item : styles.itemEnd}>
-            <div className={styles.left}>
-                <div className={styles.imageContainer}>
-                    {icon}
+        <animated.div ref={ref} style={style} {...other}>
+            {React.cloneElement(children, { onClick })}
+        </animated.div>
+    );
+});
+
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "90%",
+    maxWidth: 650,
+    bgcolor: 'background.paper',
+    padding: 24,
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    border: "2px solid #9e9e9e",
+    borderRadius: 3,
+    boxShadow: 24,
+    p: 4,
+};
+
+
+
+const Card: React.FC<cardProp> = ({ isLast, url, name, icon, techStack }) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <>
+            <div className={!isLast ? styles.item : styles.itemEnd}>
+                <div className={styles.left}>
+                    <div className={styles.imageContainer}>
+                        {icon}
+                    </div>
+                    <p className={styles.pname}>{name}</p>
                 </div>
-                <p className={styles.pname}>{name}</p>
+                <button className={styles.link} onClick={handleOpen}>
+                    <p className={styles.linkText}>
+                        Take a look
+                    </p>
+                    <ArrowRight width={12} height={12} />
+                </button>
             </div>
-            <Link href={url} target="_blank" className={styles.link}>
-                <p className={styles.linkText}>
-                    Take a look
-                </p>
-                <ArrowRight width={12} height={12} />
-            </Link>
-        </div>
+
+            <Modal
+                aria-labelledby="spring-modal-title"
+                aria-describedby="spring-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        TransitionComponent: Fade,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={modalStyle}>
+                        <div className={styles.modalHeader}>
+                            <div className={styles.htop}>
+                                <p className={styles.pname}>{name}</p>
+                                <Cancel width={25} height={25} color="#000" style={{ cursor: "pointer" }} onClick={handleClose} />
+                            </div>
+                            <div className={styles.hbottom}>
+                                {techStack.map((item,idx)=>(
+                                    <div key={idx} className={styles.headerIcon}>{item}</div>
+                                ))}
+                            </div>
+
+                        </div>
+                    </Box>
+                </Fade>
+            </Modal>
+        </>
     )
 }
 
--- src/components/Projects/d_projects.tsx
+++ src/components/Projects/d_projects.tsx
@@ -3,47 +3,96 @@ import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import CloudIcon from '@mui/icons-material/Cloud';
 import TryIcon from '@mui/icons-material/Try';
 import { ReactNode } from 'react';
-import { PageEdit } from 'iconoir-react';
+import { Npm, PageEdit } from 'iconoir-react';
+import { FaReact, FaNodeJs, FaHtml5, FaBootstrap, FaNpm, FaMailBulk } from "react-icons/fa";
+import { BiLogoMongodb } from 'react-icons/bi'
+import { SiExpress } from 'react-icons/si'
+import { IoLogoPwa } from 'react-icons/io5'
+import { Chip } from '@mui/material'
 
 export interface cardProp {
     isLast: boolean;
     url: string;
     name: string;
     icon: ReactNode
+    description: string,
+    techStack: ReactNode[],
 };
 
-const size = 22;
+const size = 22, tsize = 22;
+const iconProps = { size: tsize };
 
 const projectdata: cardProp[] = [
     {
         "url": "/authify",
         "name": "Authify",
         "isLast": false,
-        "icon": <GppGoodIcon width={size} height={size} />
+        "icon": <GppGoodIcon width={size} height={size} />,
+        "techStack": [
+            <FaNodeJs key={4} {...iconProps} />,
+            <SiExpress key={5} {...iconProps} />,
+            <BiLogoMongodb key={6} {...iconProps} />,
+            <Npm key={7} {...iconProps} />,
+        ],
+        "description": ""
     },
     {
         "url": "/axonify",
         "name": "Axonify",
         "isLast": false,
-        "icon": <TryIcon width={size} height={size} />
+        "icon": <TryIcon width={size} height={size} />,
+        "techStack": [
+            <FaNodeJs key={4} {...iconProps} />,
+            <SiExpress key={5} {...iconProps} />,
+            <BiLogoMongodb key={6} {...iconProps} />,
+            <Npm key={7} {...iconProps} />,
+        ],
+        "description": ""
     },
     {
         "url": "/gencert",
         "name": "Certify",
         "isLast": false,
-        "icon": <WorkspacePremiumIcon width={size} height={size} />
+        "icon": <WorkspacePremiumIcon width={size} height={size} />,
+        "techStack": [
+            <FaMailBulk key={3} {...iconProps} />,
+            <FaNodeJs key={4} {...iconProps} />,
+            <SiExpress key={5} {...iconProps} />,
+            <BiLogoMongodb key={6} {...iconProps} />,
+            <Npm key={7} {...iconProps} />,
+        ],
+        "description": ""
     },
     {
         "url": "/mausam",
         "name": "Mausam",
         "isLast": false,
         "icon": <CloudIcon width={size} height={size} />
+        ,
+        "techStack": [
+            <FaHtml5 key={1} {...iconProps} />,
+            <FaBootstrap key={2} {...iconProps} />,
+            <FaReact key={3} {...iconProps} />,
+            <IoLogoPwa key={4} {...iconProps} />,
+        ],
+        "description": ""
     },
     {
         "url": "/notify",
         "name": "Notify",
         "isLast": true,
         "icon": <PageEdit width={size} height={size} />
+        ,
+        "techStack": [
+            <FaHtml5 {...iconProps} key={1} />,
+            <FaBootstrap {...iconProps} key={2} />,
+            <FaReact {...iconProps} key={3} />,
+            <FaNodeJs {...iconProps} key={4} />,
+            <SiExpress {...iconProps} key={5} />,
+            <BiLogoMongodb {...iconProps} key={6} />,
+            <IoLogoPwa {...iconProps} key={7} />,
+        ],
+        "description": ""
     }
 ]
 
