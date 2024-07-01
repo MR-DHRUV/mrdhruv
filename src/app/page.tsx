55fd9f24136cbbfffbc507b888089fe06fda9039
dhruv <dhruvgupta742@gmail.com>
2024-07-01 23:55:02 +0530

    Parallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import About from '@/components/About/page'
 import Experience from '@/components/Experience/page'
 import Projects from '@/components/Projects/page'
@@ -8,31 +10,49 @@ import Education from '@/components/Education/page'
 import Socials from '@/components/Socials/page'
 import DSA from '@/components/DSA/page'
 import Contact from '@/components/Contact/page'
+import Reveal from "./Reveal"
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.mainContainer}>
-        <div className={styles.gradientTop}>
-          <img src="/static/gradient.png" alt="gradient" className={styles.gradient} />
-        </div>
-        <Header />
-        <About />
-        <Education />
-        <Socials />
-        <Experience />
-        <DSA />
-        <Projects />
-        <Hobbies />
-        <Contact />
-        <div className={styles.gradientbottom}>
-          <img src="/static/gradient.png" alt="gradient" className={styles.gradient} />
-        </div>
-      </div>
-      {/* <div className={styles.gradientbottom}></div> */}
-    </> 
-  )
+    return (
+        <>
+            <div className={styles.mainContainer}>
+                <div className={styles.gradientTop}>
+                    <img src="/static/gradient.png" alt="gradient" className={styles.gradient} />
+                </div>
+
+                <Header />
+                <Reveal>
+                    <About />
+                </Reveal>
+                <Reveal>
+                    <Education />
+                </Reveal>
+                <Reveal>
+                    <Socials />
+                </Reveal>
+                <Reveal>
+                    <Experience />
+                </Reveal>
+                <Reveal>
+                    <DSA />
+                </Reveal>
+                <Reveal>
+                    <Projects />
+                </Reveal>
+                <Reveal>
+                    <Hobbies />
+                </Reveal>
+                <Reveal>
+                    <Contact />
+                </Reveal>
+
+                <div className={styles.gradientbottom}>
+                    <img src="/static/gradient.png" alt="gradient" className={styles.gradient} />
+                </div>
+            </div>
+            {/* <div className={styles.gradientbottom}></div> */}
+        </>
+    )
 }
 
- 
-       
+
