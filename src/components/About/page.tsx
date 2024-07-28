6979e966c1365a5e9e97d82400977ac084507303
dhruv <dhruvgupta742@gmail.com>
2024-07-28 17:40:36 +0530

    Content update

--- src/components/About/page.tsx
+++ src/components/About/page.tsx
@@ -31,7 +31,7 @@ export default function About() {
             </h1>
             <Tags />
             <p className="desc-l">
-                I&#39;m a skilled software developer with expertise in <strong>Next.js, React.js, and Node.js</strong>. Leveraging my strong knowledge of <strong>Data Structures and Algorithms</strong>, I enjoy optimizing the performance and functionality of web apps and backend services. My passion lies in creating seamless user experiences rather than just web pages. I have collaborated on numerous projects with <strong>startups, local businesses and campus clubs</strong>. Let&#39;s connect and bring our ideas to life!
+                I&#39;m a skilled software developer with expertise in <span className="fw-med">Next.js, React.js, and Node.js</span>. Leveraging my strong knowledge of <span className="fw-med">Data Structures and Algorithms</span>, I enjoy optimizing the performance and functionality of web apps and backend services. My passion lies in creating seamless user experiences rather than just web pages. I have collaborated on numerous projects with <span className="fw-med">startups, local businesses and campus clubs</span>. Let&#39;s connect and bring our ideas to life!
             </p>
         </div>
     )
