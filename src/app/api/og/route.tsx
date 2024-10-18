cc36d5c669082fdc7288197830ad4c53b65237bb
dhruv <dhruvgupta742@gmail.com>
2024-10-18 14:09:41 +0530

    Dynamic og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,56 +1,52 @@
 import { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
+import { projects } from "#site/content"
 
 export const runtime = "edge";
-
-
 export async function GET(req: NextRequest) {
     try {
         const { searchParams } = req.nextUrl;
         const title = searchParams.get("title");
-        console.log("searchParams", searchParams);
         if (!title) {
             return new Response("Title is required", { status: 400 });
         }
 
-        const heading = title.length > 140 ? title.substring(0, 140) + "..." : title;
+        const project = projects.find((project) => project.title === title);
+        if (!project) {
+            return new Response("Project not found", { status: 404 });
+        }
+
         return new ImageResponse((
-            <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
-                <div tw="flex items-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <path d="M4 11a9 9 0 0 1 9 9" />
-                        <path d="M4 4a16 16 0 0 1 16 16" />
-                        <circle cx="5" cy="19" r="1" />
-                    </svg>
-                    <p tw="ml-2 font-bold text-2xl">JollyBlog</p>
+            <div tw="flex-shrink-0 m-4 bg-[#f5f5f5] w-[700px] flex flex-col justify-start items-start p-6 content-start flex-wrap rounded-lg border border-gray-400 border-lg justify-center">
+                <div tw="text-[40px] mb-3 mt-0 font-extrabold">
+                    {project.title}
                 </div>
-                <div tw="flex flex-col flex-1 py-10">
-                    <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
-                        BLOG POST
-                    </div>
-                    <div tw="flex text-[80px] font-bold text-[50px]">{heading}</div>
+                <div tw="flex flex-row flex-wrap items-center mb-3">
+                    {project.tags.map((tag) => (
+                        <span tw="bg-zinc-200 text-zinc-700 text-xs font-medium px-2.5 py-0.5 rounded mr-2" key={tag} >{tag}</span>
+                    ))}
                 </div>
-                <div tw="flex items-center w-full justify-between">
-                    <div tw="flex text-xl">mrdhruv.co</div>
-                    <div tw="flex items-center text-xl">
-                        <div tw="flex ml-2">github.com/mr-dhruv</div>
-                    </div>
+                <p tw="desc-l !text-[18.7px]">
+                    {project.description}
+                </p>
+                <img src={`https://www.mrdhruv.co${project.image}`} tw="w-full rounded-lg border" width={630} height={235} />
+                <div tw="flex flex-row justify-between w-full mt-6">
+                    <a href={project.github} tw="text-md font-medium text-blue-500" target="_blank">
+                        mrdhruv
+                    </a>
+                    <a href={project.github} tw="text-md font-medium text-blue-500" target="_blank">
+                        mrdhruv.co/{project.slug}
+                    </a>
                 </div>
-            </div>),
+            </div>
+        ),
             {
-                width: 1200,
-                height: 630,
+                width: 732,
+                height: 540,
             }
         )
     } catch (error) {
+        console.error(error);
         return new Response("Cant generate a og image", { status: 500 });
     }
 }
