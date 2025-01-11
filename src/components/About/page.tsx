79b177afcadec4d1e5414362309bbfbd497c6b07
Dhruv <dhruvgupta742@gmail.com>
2025-01-11 15:54:17 +0530

    Intro

--- src/components/About/page.tsx
+++ src/components/About/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
             </h1>
             <Tags />
             <p className="desc-l">
-                I&#39;m a skilled software developer with expertise in <span className="fw-med">Next.js, React.js, and Node.js</span>. Leveraging my strong knowledge of <span className="fw-med">Data Structures and Algorithms</span>, I enjoy optimizing the performance and functionality of web apps and backend services. My passion lies in creating seamless user experiences rather than just web pages. I have collaborated on numerous projects with <span className="fw-med">startups, local businesses and campus clubs</span>. Let&#39;s connect and bring our ideas to life!
+            Hi, I&#39;m Dhruv—a curious and passionate software developer who loves creating innovative solutions that make an impact. Proficient in <span className="fw-med">Next.js, React.js, and Node.js</span>, I excel at building high-performance applications and delivering seamless user experiences. My strong grasp of <span className="fw-med">Data Structures and Algorithms</span> helps me design scalable, reliable software. With experience working alongside startups, local businesses, and campus clubs, I thrive in dynamic teams. Currently, I&#39;m interning at <span className="fw-med">GoDaddy</span>. Beyond coding, I enjoy traveling, cars, and exploring the latest tech. Let’s connect and build something great together!
             </p>
         </div>
     )
